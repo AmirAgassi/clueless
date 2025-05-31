70f4e8f485810c21965ef6eb46e8043fd144c024
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2025-05-31 19:02:58 -0400

    feat: add clueless.exe release

--- clueless/compile.bat
+++ clueless/compile.bat
@@ -1,3 +1,2 @@
-@echo off
 C:\msys64\ucrt64\bin\g++.exe -m64 src\commandline_checks.cpp src\filesystem_checks.cpp src\main.cpp src\process_checks.cpp src\registry_checks.cpp src\utils.cpp src\window_checks.cpp -o clueless.exe -static-libgcc -static-libstdc++ -Wall -O2 -lpsapi -ladvapi32 2>&1
 clueless.exe
