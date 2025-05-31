5a01f7cedad38e83393136edcf86a68fa6f48ddc
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2025-05-31 18:47:23 -0400

    feat: compile in 64bit

--- clueless/compile.bat
+++ clueless/compile.bat
@@ -1,2 +1,3 @@
 @echo off
-g++ src\*.cpp -o clueless.exe -static-libgcc -static-libstdc++ -Wall -O2 -lpsapi -ladvapi32
+C:\msys64\ucrt64\bin\g++.exe -m64 src\commandline_checks.cpp src\env_checks.cpp src\filesystem_checks.cpp src\main.cpp src\process_checks.cpp src\registry_checks.cpp src\utils.cpp src\window_checks.cpp -o clueless.exe -static-libgcc -static-libstdc++ -Wall -O2 -lpsapi -ladvapi32 2>&1
+clueless.exe
